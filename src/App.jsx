d438b00aeb04f3c24d9ab10df82b99af6d098da2
Houssem <sekrihoussem.work@gmail.com>
2023-03-31 02:17:44 +0100

    adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import { Box } from "@mui/material";
 import OptionsTab from "./components/OptionsTab";
 import Container from "@mui/material/Container";
 import LocationCards from "./components/LocationCards";
+import Footer from "./components/Footer";
+import MobileFooter from "./components/MobileFooter";
+import { displayOnDesktop } from "./themes/commonTheme";
 
 function App() {
   return (
@@ -20,6 +23,20 @@ function App() {
           <Container maxWidth="xl" sx={{ mb: 3 }}>
             <LocationCards />
           </Container>
+        </Box>
+        <Box
+          sx={{
+            display: { xs: "flex", md: "none" },
+          }}
+        >
+          <MobileFooter />
+        </Box>
+        <Box
+          sx={{
+            display: { xs: "none", md: "flex" },
+          }}
+        >
+          <Footer />
         </Box>
       </Box>
     </AppthemeProvider>
