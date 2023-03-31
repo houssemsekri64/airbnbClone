b8c505b13a253c7923c53398a4e01857c9dfd335
Houssem <sekrihoussem.work@gmail.com>
2023-03-31 01:35:20 +0100

    adding carousel

--- src/components/LocationCards.jsx
+++ src/components/LocationCards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import { locations as cardLocations } from "../data/mock-data";
-// import CarouselCard from "./CarouselCard";
+import CarouselCard from "./CarouselCard";
 
 function LocationCards() {
   const [cards] = React.useState(cardLocations);
@@ -11,8 +11,8 @@ function LocationCards() {
       <Grid container rowSpacing={3} columnSpacing={3}>
         {cards.map((card) => {
           return (
-            <Grid key={card.id} xs={12} md={4} lg={3}>
-              {card.location}
+            <Grid key={card.id} xs={12} md={4} lg={3} item>
+              <CarouselCard location={card} />
             </Grid>
           );
         })}
